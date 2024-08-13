97a85522095cb4803b3614f744fe58e5dfe9bf84
RZRexton <ooiruizhe@gmail.com>
2024-08-14 01:04:04 +0800

    Updated latest streamlit Uncle Roger AINewbies GPT

--- jejubot.py
+++ jejubot.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import streamlit.components.v1 as components
 import os
 import base64
 from datetime import datetime
@@ -37,10 +36,9 @@ def initialize_session_state():
 
 def generate_response(prompt, lorax_client, adapter_id):
     try:
-        # Simulate typing effect
-        with st.spinner("AINewbies is thinking..."):
-            response = lorax_client.generate(prompt, adapter_id=adapter_id, max_new_tokens=30000)
-
+        with spinner_placeholder:
+            with st.spinner("AINewbies is thinking..."):
+                response = lorax_client.generate(prompt, adapter_id=adapter_id, max_new_tokens=30000)
         return response.generated_text
     except Exception as e:
         print("Error generating response:", e)
@@ -48,48 +46,20 @@ def generate_response(prompt, lorax_client, adapter_id):
 
 def expand_abbreviations(text):
     abbreviation_dict = {
-        "u": "you",
-        "r": "are",
-        "pls": "please",
-        "msg": "message",
-        "btw": "by the way",
-        "lol": "laugh out loud",
-        "rsvp": "please reply",
-        "asap": "as soon as possible",
-        "am": "morning",
-        "pm": "afternoon",
-        "lmk": "let me know",
-        "brb": "be right back",
-        "dob": "date of birth",
-        "cc": "carbon copy",
-        "bcc": "blind carbon copy",
-        "tba": "to be announced",
-        "tbc": "to be confirmed",
-        "eta": "estimated time of arrival",
-        "tgif": "thank God it's Friday",
-        "fomo": "fear of missing out",
-        "imo": "in my opinion",
-        "imho": "in my humble opinion",
-        "n/a": "not available",
-        "aka": "also known as",
-        "diy": "do it yourself",
-        "ty": "thank you",
-        "fyi": "for your information",
-        "idk": "I don't know",
-        "ttyl": "talk to you later",
-        "omg": "oh my God",
-        "smh": "shaking my head",
-        "tmi": "too much information",
-        "yolo": "you only live once",
-        "jk": "just kidding",
-        "tbh": "to be honest",
-        "bff": "best friends forever",
-        "gtg": "got to go",
-        "nvm": "never mind",
-        "idc": "I don't care",
-        "irl": "in real life",
-        "dm": "direct message",
-        "ttys": "talk to you soon"
+        "u": "you", "r": "are", "pls": "please", "msg": "message", 
+        "btw": "by the way", "lol": "laugh out loud", "rsvp": "please reply", 
+        "asap": "as soon as possible", "am": "morning", "pm": "afternoon", 
+        "lmk": "let me know", "brb": "be right back", "dob": "date of birth", 
+        "cc": "carbon copy", "bcc": "blind carbon copy", "tba": "to be announced", 
+        "tbc": "to be confirmed", "eta": "estimated time of arrival", "tgif": "thank God it's Friday", 
+        "fomo": "fear of missing out", "imo": "in my opinion", "imho": "in my humble opinion", 
+        "n/a": "not available", "aka": "also known as", "diy": "do it yourself", 
+        "ty": "thank you", "fyi": "for your information", "idk": "I don't know", 
+        "ttyl": "talk to you later", "omg": "oh my God", "smh": "shaking my head", 
+        "tmi": "too much information", "yolo": "you only live once", "jk": "just kidding", 
+        "tbh": "to be honest", "bff": "best friends forever", "gtg": "got to go", 
+        "nvm": "never mind", "idc": "I don't care", "irl": "in real life", 
+        "dm": "direct message", "ttys": "talk to you soon"
     }
     words = text.split()
     expanded_text = " ".join([abbreviation_dict.get(word.lower(), word) for word in words])
@@ -101,15 +71,11 @@ def on_click_callback():
     if not human_prompt:
         return
 
-    # Expand abbreviations
     expanded_prompt = expand_abbreviations(human_prompt)
-
-    # Convert to sentence case
-    new_human_prompt = expanded_prompt.lower()
-    new_human_prompt = new_human_prompt[0].upper() + new_human_prompt[1:]
+    new_human_prompt = expanded_prompt.lower().capitalize()
 
     if not new_human_prompt.endswith(('.', '?', '!')):
-        if new_human_prompt.lower().startswith(('who', 'what', 'where', 'when', 'why', 'how', 'whom', 'whose','which')):
+        if new_human_prompt.lower().startswith(('who', 'what', 'where', 'when', 'why', 'how', 'whom', 'whose', 'which')):
             new_human_prompt += '?'
         else:
             new_human_prompt += '.'
@@ -123,7 +89,7 @@ def on_click_callback():
     llm_response = generate_response(new_human_prompt, lorax_client, adapter_id)
     st.session_state.history.append(Message("ai", llm_response, timestamp))
 
-    st.session_state.human_prompt = "" 
+    st.session_state.human_prompt = ""
 
 def display_chat_history():
     chat_placeholder.empty()
@@ -137,7 +103,7 @@ def display_chat_history():
 def display_human_message(message, timestamp):
     div = f"""
     <div style="display: flex; align-items: flex-end; justify-content: flex-end; margin-bottom: 10px;">
-        <div class="chat-bubble human-bubble" style="background-color: #d7c4fd; padding: 10px; border-radius: 10px; max-width: 70%; margin-right: 10px;">
+        <div class="chat-bubble human-bubble" style="background-color: #d7c4fd; padding: 10px; border-radius: 10px; max-width: 70%; margin-right: 10px;margin-top: 10px;">
             &#8203;{message}
             <div style="font-size: 10px; color: #ececec; text-align: right;">{timestamp}</div>
         </div>
@@ -158,21 +124,19 @@ def display_bot_message(message, timestamp):
     """
     st.markdown(div, unsafe_allow_html=True)
 
-# Load custom CSS
 load_css()
 initialize_session_state()
 
-# Title of the app
-st.title("AINewbies Tourism GPT🗺️⁀જ✈︎")
+st.markdown("<h1 style='text-align: center;'>AINewbies Tourism GPT🗺️⁀જ✈︎</h1>", unsafe_allow_html=True)
 
-# Placeholder for chat interaction
+st.markdown("<div style='height: 150px;'></div>", unsafe_allow_html=True)
+
 chat_placeholder = st.container()
 
-# Display the existing chat history
 display_chat_history()
 
-# Prompt for user input
 with st.form("chat-form"):
+    st.markdown('<div class="fixed-chat-form">', unsafe_allow_html=True)
     st.markdown("**Chat**")
     cols = st.columns((6, 1))
     cols[0].text_input(
@@ -187,3 +151,12 @@ with st.form("chat-form"):
         type="primary", 
         on_click=on_click_callback, 
     )
+    spinner_placeholder = st.empty()  # Placeholder for spinner
+    st.markdown('</div>', unsafe_allow_html=True)
+
+
+st.markdown("""
+    <div class="footer" style="position: fixed; bottom: 0; left: 0; width: 100%; text-align: center; background-color: #f8f9fa; box-shadow: 0 -2px 5px rgba(0,0,0,0.1);">
+        <p>&copy; 2024 AINewbies Tourism GPT. All rights reserved.</p>
+    </div>
+    """, unsafe_allow_html=True)
